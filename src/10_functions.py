ab106cae982bcdd71d2a3bde78c082b2d08d86d2
vebradev <justinas@vebra.dev>
2020-02-10 15:15:14 +0000

    implement 10_functions.py

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,6 +1,15 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
-# YOUR CODE HERE
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+
+print(is_even(2))  # True
+print(is_even(3))  # False
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
@@ -8,5 +17,4 @@ num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
-# YOUR CODE HERE
-
+print("Even!" if is_even(num) == True else "Odd!")
